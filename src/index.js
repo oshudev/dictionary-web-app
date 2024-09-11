6bfcbd1b1c7336d37d4fb0840a217cc18acbbe9b
oshudev <99813212+eurus1108@users.noreply.github.com>
2024-09-11 20:07:08 +0800

    feat: color theme switcher prototype

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './scss/main.scss';
+
+// TODO: add watch functionality when users is in dark mode and set the correct toggle state
+const toggle = document.querySelector('[data-theme-toggle]');
+const root = document.querySelector('html');
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active');
+
+  root.setAttribute(
+    'data-color-theme',
+    toggle.classList.contains('active') ? 'dark' : 'light',
+  );
+});
